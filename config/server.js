d1ea63423c66554864826ed72e0d05a2424ac706
MacBook <unicoder11@gmail.com>
2021-03-30 21:12:33 -0300

    Strapi started

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2a1d603366aedd6cd402c023724d63b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd7d90613-563b-4f1a-beec-5a452ff1f917'
+};
